458d973afe21851fdf03183cf3d713a3e17f181e
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-12-02 19:41:58 +0530

    day2

---
+++ day0.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func day0_1(input string) {
+	// lines := strings.Split(strings.TrimSpace(input), "\n")
+	output := 0
+
+	fmt.Println("Output Day 1 Part 1", output)
+}
+
+func day0_2(input string) {
+	// lines := strings.Split(strings.TrimSpace(input), "\n")
+	output := 0
+
+	fmt.Println("Output Day 1 Part 2", output)
+}
--- main.go
+++ main.go
@@ -45,10 +45,10 @@ func getInput(day int) string {
 func main() {
 	godotenv.Load(".env")
 
-	day := 1
+	day := 2
 	input := getInput(day)
-	// fmt.Println(input
+	// fmt.Println(input)
 	// Your solution logic here
-	day1_1(input)
-	day1_2(input)
+	day2_1(input)
+	day2_2(input)
 }
