138f06aed0c96a732dc53ce192304a1f54a5709a
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-12-06 21:19:02 +0530

    day 6 part 1

--- main.go
+++ main.go
@@ -45,10 +45,10 @@ func getInput(day int) string {
 func main() {
 	godotenv.Load(".env")
 
-	day := 5
+	day := 6
 	input := getInput(day)
 	// fmt.Println(input)
 	// Your solution logic here
-	day5_1(input)
-	day5_2(input)
+	day6_1(input)
+	day6_2(input)
 }
